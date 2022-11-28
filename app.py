2a95cee6685e1782d8de88b470ac36347ebe1196
Martin Rauscher <martin@ultimatetournament.io>
2022-11-28 21:09:07 +0100

    make input and output a param

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 import time
 import json
 import torch
@@ -41,14 +42,11 @@ def inference(model_inputs:dict) -> dict:
         secret_key=model_inputs.get("S3_SECRET", os.getenv("S3_SECRET")),
         region=model_inputs.get("S3_REGION", os.getenv("S3_REGION"))
     )
-    data_file_id = model_inputs.get('file_id', None)
+    s3_input_path = model_inputs.get('input_path', None)
+    s3_output_path = model_inputs.get('output_path', None)
 
-    if data_file_id is None:
+    if s3_input_path is None:
         return {error: "no input"}
-
-    inputS3object = 'inputs/'+data_file_id+'.zip'
-    print(f"downloading {inputS3object}")
-    s3client.fget_object(s3bucket, inputS3object, 'sks.zip')
 
     # Setup concepts_list
     concepts_list = [
@@ -59,6 +57,13 @@ def inference(model_inputs:dict) -> dict:
             "class_data_dir":       "data/person"
         }
     ]
+    train_path = concepts_list[0]["instance_data_dir"]
+
+    print(f"downloading {s3_input_path}")
+    objects = s3client.list_objects(s3bucket, prefix=s3_input_path)
+    for obj in objects:
+        s3client.fget_object(s3bucket, obj.object_name, pathlib.posixpath.join(train_path, pathlib.posixpath.basename(obj.object_name)))
+
     # 'class_data_dir' contains regularization images
     # 'instance_data_dir' is where training images go
     for c in concepts_list:
@@ -66,11 +71,6 @@ def inference(model_inputs:dict) -> dict:
     # Create concept file
     with open("concepts_list.json", "w") as f:
         json.dump(concepts_list, f, indent=4)
-
-    #Unzip training images
-    train_path = concepts_list[0]["instance_data_dir"]
-    with zipfile.ZipFile('sks.zip', 'r') as f:
-        f.extractall('data/sks')
     
     #Call training script
     steps = 1200 # see train.sh
@@ -84,12 +84,11 @@ def inference(model_inputs:dict) -> dict:
     shutil.make_archive("weights", "zip", f"stable_diffusion_weights/{steps}")
 
     uploadStart = time.monotonic_ns()
-    weightsBucketFile = f'weights/{data_file_id}_{uploadStart}.zip'
-    print(f"uploading {weightsBucketFile}")
+    print(f"uploading {s3_output_path}")
     s3client.fput_object(
-        s3bucket, weightsBucketFile, "weights.zip", part_size=100*1024*1024,
+        s3bucket, s3_output_path, "weights.zip", part_size=100*1024*1024,
     )
     print(f"finished uploading in {(time.monotonic_ns() - uploadStart)/1_000_000_000}s")
 
     # Return the results as a dictionary
-    return {'response': str(weightsBucketFile)}
+    return {'response': str(s3_output_path)}
